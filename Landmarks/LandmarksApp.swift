473ba678d3144ffea9730f4c4140c958b6c609a6
alexShekotev <shekotev.a@ext-system.com>
2024-03-19 10:31:40 +0700

    les 1 - Creating and combining views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by iOS Developer on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
