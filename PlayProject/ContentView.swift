4c5e7d90d3f2069d37d4815bceec03c20d03b202
alexShekotev <shekotev.a@ext-system.com>
2024-03-19 09:35:08 +0700

    Initial Commit

---
+++ PlayProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlayProject
+//
+//  Created by iOS Developer on 19.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlayProject/PlayProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayProjectApp.swift
+//  PlayProject
+//
+//  Created by iOS Developer on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlayProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
