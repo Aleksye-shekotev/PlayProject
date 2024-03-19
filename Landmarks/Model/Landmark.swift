c82d02924047b396a1317dc66e07026ebcb22650
alexShekotev <shekotev.a@ext-system.com>
2024-03-19 11:06:27 +0700

    les 2 - Building lists and navigation

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,34 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by iOS Developer on 19.03.2024.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Decodable, Hashable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    private var coordinates: Coordinates
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+}
---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by iOS Developer on 19.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
